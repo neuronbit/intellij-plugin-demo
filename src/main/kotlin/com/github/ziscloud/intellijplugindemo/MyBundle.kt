5b3c11b2a9921db1dca742a83b7b3604cb6bbb93
GitHub Action <action@github.com>
2020-08-02 05:29:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/ziscloud/intellijplugindemo/MyBundle.kt
+++ src/main/kotlin/com/github/ziscloud/intellijplugindemo/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ziscloud.intellijplugindemo
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/ziscloud/intellijplugindemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ziscloud.intellijplugindemo.services
+
+import com.github.ziscloud.intellijplugindemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
